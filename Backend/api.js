fcc301f0bd5b953f4dd4821785560a55a35f136c
edp-darios <dstuebi@edp.ch>
2021-11-25 14:38:51 +0100

    First Commit

---
+++ Backend/api.js
@@ -0,0 +1,14 @@
+'use strict'
+import {Router} from "https://deno.land/x/oak@v6.3.1/mod.ts";
+import { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts";
+
+let cards = {
+    id: v4.generate(), beschreibung: "Das ist eine Karte"
+}
+
+const router = new Router();
+
+router  
+    .get("/api/cards", context => context.response.body = cards);
+
+export const apiRoutes = router.routes();
---
+++ webserver.js
@@ -0,0 +1,10 @@
+'use strict'
+import { Application } from "https://deno.land/x/oak@v6.3.1/mod.ts";
+import {apiRoutes} from "./Backend/api.js";
+import { frontendFiles } from "./Backend/fileserver.js";
+
+const app = new Application();
+
+app.use(apiRoutes);
+app.use(frontendFiles);
+app.listen({port: 8000});
