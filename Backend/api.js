46578ee3285ff09f1bee5a27a3a07ff8570c3398
edp-darios <dstuebi@edp.ch>
2021-12-02 13:33:08 +0100

    addons from class backend

--- Backend/api.js
+++ Backend/api.js
@@ -10,6 +10,14 @@ let cards = [
 const router = new Router();
 
 router  
-    .get("/api/cards", context => context.response.body = cards);
+    .get("/api/cards", context => context.response.body = cards)
+    .get("/api/id", context => context.response.body = v4.generate())
+    .post("/api/cards", async context => {
+        const newCard = await context.response.body({type: "json"}).value;
+        console.log("requestBody: ", newCard);
+        cards = [
+            ...cards, newCard
+        ];
+    })
 
 export const apiRoutes = router.routes();
