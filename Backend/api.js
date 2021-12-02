cae2a0ff4bba433f825c855f65d334f5fffde744
edp-darios <dstuebi@edp.ch>
2021-12-02 13:27:33 +0100

    Bug fixing

--- Backend/api.js
+++ Backend/api.js
@@ -2,9 +2,10 @@
 import {Router} from "https://deno.land/x/oak@v6.3.1/mod.ts";
 import { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts";
 
-let cards = {
-    id: v4.generate(), beschreibung: "Das ist eine Karte"
-}
+let cards = [
+    {id: v4.generate(), beschreibung: "One"},
+    {id: v4.generate(), beschreibung: "Two"}
+]
 
 const router = new Router();
 
